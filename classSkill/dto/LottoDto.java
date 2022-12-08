2cfdd7e6a33ba3eacf20f0bebd7f0845e60acc1c
오승윤 <>
2022-12-08 19:52:48 +0900

    refactor (dto) : 설명 추가

--- classSkill/dto/LottoDto.java
+++ classSkill/dto/LottoDto.java
@@ -7,6 +7,12 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * dto란 데이터 변환 계층으로 컨트롤러에서 값을 사용자(mvc에선 view)에게 전달할 때 사용한다
+ * 원본 객체를 받아 dto로 변환하는 메서드와 getter & setter를 가진다
+ *
+ * 데이터 변환 레이어를 분리함으로써 복잡한 시스템이 관리하기 용이해진다
+ */
 public class LottoDto {
     //final
     private final Set<Integer> lottoNumbers;
